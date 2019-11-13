4883ad247df0e21dceffcc434c608bc6c2033a7e
jenseneducatiuon-mahmudulhaque-mollah <54712457+jenseneducatiuon-mahmudulhaque-mollah@users.noreply.github.com>
2019-11-13 21:37:56 +0100

    first commit

---
+++ src/MyClass.java
@@ -0,0 +1,11 @@
+public class MyClass {
+    int x = 5;
+    //String myString= "Hello World";
+
+    public static void main(String[] args) {
+        MyClass myObj = new MyClass();
+
+        System.out.println(myObj.x);
+
+    }
+}
---
+++ src/experiment/Main.java
@@ -0,0 +1,8 @@
+package experiment;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
